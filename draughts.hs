922fea17fe956d8ebd9a11bf98adb0ffae1e8807
Caio Cesar Dionello <ccdionello@gmail.com>
2019-06-02 09:32:57 -0300

    Update draughts.hs

--- draughts.hs
+++ draughts.hs
@@ -1,19 +1,23 @@
+-- O jogo é representado por um tabuleiro quadrado de valores Maybe Piece, onde Piece pode assumir
+-- os valores Black e White.
 data Piece = Black | White deriving (Show)
 
 type Board = [[Maybe Piece]]
 
---emptyBoard :: Int -> Board
---emptyBoard n = [[ Nothing | x <- 
+-- o tabuleiro vazio é inicializado com todos os valores Nothing
+emptyBoard :: Int -> Board
+emptyBoard n = [[ Nothing | x <- [1..n]] | y <- [1..n]]
 
 type Coord = (Int,Int)
 
 type Move = (Coord, Coord)
 
-notIn :: Coord -> Board -> Bool
-coord `notIn` Board = if (fst coord >l) || (snd coord >l)
+-- isIn: retorna True caso a coordenada esteja dentro do tabuleiro. Caso contrário, retorna False
+isIn :: Coord -> Board -> Bool
+coord `isIn` board = if (fst coord >l) || (snd coord >l)
                       then False
                       else True
-                      where l = length Board
+                      where l = length board
 
 isValid :: Move -> Board -> Bool
 isValid move board | initial `notIn` Board = False
