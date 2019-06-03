4a1c51563a41c1517ec0f1b042dc93515692a6f2
Caio Cesar Dionello <ccdionello@gmail.com>
2019-06-03 15:58:05 -0300

    Create TicTacToe.hs

---
+++ TicTacToe.hs
@@ -0,0 +1,34 @@
+data Mark  = Nought | Cross deriving (Show, Eq)
+type Board = [[Maybe Mark]]
+
+emptyBoard :: Int -> Board
+emptyBoard n = [[ Nothing | x <- [1..n]] | y <- [1..n]]
+-- cria um tabuleiro vazio de tamanho n x n
+
+type Coord = (Int, Int)
+type Move = (Mark, Coord)
+
+notIn :: Coord -> Board -> Bool
+coord `notIn` board 
+        | (fst coord >= l) = True
+        | (snd coord >= l) = True
+        | otherwise        = False
+        where l = length board
+
+spaceAt :: Coord -> Board -> Maybe Mark
+spaceAt (x,y) board = (board !! x) !! y
+
+isValid :: Move -> Board -> Bool
+isValid (mk,cd) board
+            | cd `notIn` board            = False
+            | spaceAt cd board == Nothing = True
+            | otherwise                   = False
+-- verifica se uma jogada é válida no tabuleiro atual
+
+makeMove :: Move -> Board -> Board
+makeMove (mk,(x,y)) board
+            | isValid (mk,(x,y)) board 
+              = take x a ++ (
+-- executa uma jogada no tabuleiro
+--isOver :: Board -> Bool
+-- verifica se o jogo terminou
