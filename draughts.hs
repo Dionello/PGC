9d3b125fa197303063a90655fed3daa7dc70a723
Caio Cesar Dionello <ccdionello@gmail.com>
2019-06-10 23:00:45 -0300

    Update draughts.hs

--- draughts.hs
+++ draughts.hs
@@ -4,15 +4,14 @@ data Piece = Black | White deriving (Show, Eq)
 
 type Board = [[Maybe Piece]]
 
--- o tabuleiro vazio é inicializado com 40% das colunas intercaladas entre Just White e Nothing,
--- 20% das colunas apenas com Nothing, e 40% das colunas intercaladas entre Just Black e Nothing.
+-- o tabuleiro vazio é inicializado com todos os valores Nothing
 emptyBoard :: Int -> Board
-emptyBoard n | n < 4     = error "Please choose a number >= 4"
-             | otherwise = [[if (odd (x+y)) then Nothing 
-             else (Just White) | x <- [1..n]] | y <- [1..qntp]]
+emptyBoard n | n < 4     = error "Please choose a number >=4"
+             | otherwise = [[if (odd (x+y)) then Nothing else (Just White) | x <- [1..n]] 
+             | y <- [1..qntp]]
              ++ [[ Nothing | x <- [1..n]] | y <- [1..qnte]]
-             ++ [[if (odd (x+y)) then Nothing 
-             else (Just Black) | x <- [1..n]] | y <- [1..qntp]]
+             ++ [[if (odd (x+y)) then Nothing else (Just Black) | x <- [1..n]] 
+             | y <- [1..qntp]]
               where qntp = floor ((0.4) * (fromIntegral n))
                   qnte  = n - (2*qntp)
 
@@ -22,7 +21,6 @@ data Quadrant = Q1 | Q2 | Q3 | Q4
 data MoveType = Walk | Jump
 type Move = (MoveType, Coord, Quadrant)
 
--- Calcula a coordenada destinado de um movimento
 destination :: Move -> Coord
 destination (Walk,cd,Q1) = ((fst cd+1),(snd cd+1))
 destination (Walk,cd,Q2) = ((fst cd-1),(snd cd+1))
@@ -32,7 +30,7 @@ destination (Jump,cd,qd)
         = destination (Walk,destination (Walk,cd,qd),qd)
         --Aplica Walk 2 vezes
 
--- notIn: retorna True caso a coordenada não esteja dentro do tabuleiro, e False caso esteja.
+-- notIn: retorna True caso a coordenada esteja dentro do tabuleiro, e False caso esteja.
 notIn :: Coord -> Board -> Bool
 coord `notIn` board 
         | (fst coord >= l) = True
@@ -59,11 +57,16 @@ insertAt p (x,y) bd
         = take x bd ++ [(take y lineX ++ [p] ++ drop (y+1) lineX)] ++ drop (x+1) bd 
         where lineX = bd !! x
 
+--exchangePositions :: mudar 2 espaços da tabela...
+
 -- executa uma jogada no tabuleiro
 makeMove :: Move -> Board -> Board
-makeMove (Walk,cd,qd) bd | isValid (Walk,cd,qd)
-                   = 
-                   where = dt = destination Move
+makeMove (mv,cd,qd) bd | not (isValid (mv,cd,qd)) = error "Move is not valid"
+                       | mv == Walk = insertAt Nothing cd (insertAt piece dest bd)
+                       | otherwise  = (insertAt Nothing cd (insertAt piece dest bd))
+                   where dest = destination Move
+                         piece = spaceAt cd qd
+                         exchange 
 
 -- verifica se o jogo encerrou
 isOver :: Board -> Bool
