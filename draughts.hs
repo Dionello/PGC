d5c7b0bdb5fbbe28351f929fb44ab085c76f0f79
Caio Cesar Dionello <ccdionello@gmail.com>
2019-06-11 22:27:16 -0300

    Update draughts.hs

--- draughts.hs
+++ draughts.hs
@@ -13,19 +13,19 @@ emptyBoard n | n < 4     = error "Please choose a number >=4"
              ++ [[if (odd (x+y)) then Nothing else (Just Black) | x <- [1..n]] 
              | y <- [1..qntp]]
               where qntp = floor ((0.4) * (fromIntegral n))
-                  qnte  = n - (2*qntp)
+                    qnte  = n - (2*qntp)
 
 type Coord = (Int,Int)
 
 data Quadrant = Q1 | Q2 | Q3 | Q4
-data MoveType = Walk | Jump
+data MoveType = Walk | Jump deriving (Eq)
 type Move = (MoveType, Coord, Quadrant)
 
 destination :: Move -> Coord
-destination (Walk,cd,Q1) = ((fst cd+1),(snd cd+1))
-destination (Walk,cd,Q2) = ((fst cd-1),(snd cd+1))
-destination (Walk,cd,Q3) = ((fst cd-1),(snd cd-1))
-destination (Walk,cd,Q4) = ((fst cd+1),(snd cd-1))
+destination (Walk,cd,Q1) = ((fst cd+1),(snd cd-1))
+destination (Walk,cd,Q2) = ((fst cd-1),(snd cd-1))
+destination (Walk,cd,Q3) = ((fst cd-1),(snd cd+1))
+destination (Walk,cd,Q4) = ((fst cd+1),(snd cd+1))
 destination (Jump,cd,qd) 
         = destination (Walk,destination (Walk,cd,qd),qd)
         --Aplica Walk 2 vezes
@@ -43,7 +43,7 @@ isValid :: Move -> Board -> Bool
 isValid (mv,cd,qd) bd | cd `notIn` bd = False
                       | dest `notIn`  bd = False
                       | spaceAt (dest) bd /= Nothing  = False
-                      | (mv == Jump) && (destination (Walk,cd,qd) == Nothing) = False
+                      | (mv == Jump) && (spaceAt (destination (Walk,cd,qd)) bd == Nothing) = False
                       | otherwise = True
                         where dest = destination (mv,cd,qd)
 
@@ -57,16 +57,18 @@ insertAt p (x,y) bd
         = take x bd ++ [(take y lineX ++ [p] ++ drop (y+1) lineX)] ++ drop (x+1) bd 
         where lineX = bd !! x
 
---exchangePositions :: mudar 2 espaços da tabela...
+exchangePositions :: Coord -> Coord -> Board -> Board
+exchangePositions cd1 cd2 bd = insertAt pc2 cd1 (insertAt pc1 cd2 bd)
+                               where pc1 = spaceAt cd1 bd
+                                     pc2 = spaceAt cd2 bd
 
 -- executa uma jogada no tabuleiro
 makeMove :: Move -> Board -> Board
-makeMove (mv,cd,qd) bd | not (isValid (mv,cd,qd)) = error "Move is not valid"
-                       | mv == Walk = insertAt Nothing cd (insertAt piece dest bd)
-                       | otherwise  = (insertAt Nothing cd (insertAt piece dest bd))
-                   where dest = destination Move
-                         piece = spaceAt cd qd
-                         exchange 
+makeMove (mv,cd,qd) bd | not (isValid (mv,cd,qd) bd) = error "Move is not valid"
+                       | (mv == Walk) = exchangePositions cd dest bd
+                       | otherwise  = exchangePositions cd dest (insertAt Nothing (destination (Walk,cd,qd)) bd)
+                        where dest = destination (mv,cd,qd)
+                          
 
 -- verifica se o jogo encerrou
 isOver :: Board -> Bool
