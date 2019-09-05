aa379c6dbf8a2adb382cd8fb8cc717e8a96e7342
Caio Cesar Dionello <ccdionello@gmail.com>
2019-09-05 13:03:27 -0300

    Update game.hs

--- TicTacToe/src/game.hs
+++ TicTacToe/src/game.hs
@@ -25,10 +25,20 @@ isValid (Move cd mk) bd | not (inRange cd bd)       = False
                         | (markAt cd bd /= Nothing) = False
                         | otherwise                 = True
 
+
+insertAt :: a -> Coord -> [[a]] -> [[a]]
+insertAt a (x,y) mt 
+        = take x mt 
+        ++ [(take y lineX ++ [a] ++ drop (y+1) lineX)] 
+        ++ drop (x+1) mt
+        where lineX = mt !! x
+
 -- executa uma jogada no tabuleiro
---makeMove :: Move -> Board -> Board
---makeMove (Move cd mk) bd | isValid (Move cd mk) bd
---                           = 
+makeMove :: Move -> Board -> Board
+makeMove (Move cd mk) bd | isValid (Move cd mk) bd
+                         = insertAt (pure mk) cd bd
+                         | otherwise = bd
 
 -- verifica se o jogo terminou
---isOver :: Board -> Bool
+isOver :: Board -> Bool
+isOver bd = not (or (map (Nothing `elem`) bd) )
