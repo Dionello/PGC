41ea959c00c5d3b361912176df0a91d89d94e7ce
Caio Cesar Dionello <ccdionello@gmail.com>
2019-09-09 19:32:27 -0300

    Jogo para 2 players humanos

--- TicTacToe/src/Main.hs
+++ TicTacToe/src/Main.hs
@@ -1,5 +1,30 @@
 module Main where
 
-main :: IO ()
+import Game
+
+--printGrid :: Board -> [IO ()]
+--printGrid bd = [print line | line <- bd]
+
 main = do
-  putStrLn "hello world"
+  putStrLn "Tic Tac Toe"
+  putStrLn "Choose the board size"
+  nl <- getLine
+  let n = (read nl :: Int) 
+      board = emptyBoard n
+  print board
+  gameLoop board Cross
+  
+changePlayer :: Mark -> Mark
+changePlayer Cross = Nought
+changePlayer Nought = Cross 
+
+gameLoop :: Board -> Mark -> IO ()
+gameLoop bd mk = do
+        putStrLn ("make a play " ++ (show mk) )
+        cd <- getLine
+        let cd' = (read cd :: (Int,Int))
+            newbd = makeMove (Move cd' mk) bd
+        print newbd
+        if (isOver newbd) 
+                then (putStrLn ((show mk) ++ " is winner")) 
+                else (gameLoop newbd (changePlayer mk))
